c76470240a83d08124f4ce4757a5477a5596eeea
MUHAMMAD SAUD <saudshahid646@gmail.com>
2023-12-30 01:04:44 +0500

    updation

--- index.js
+++ index.js
@@ -1,69 +1,92 @@
-const urlg = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IemWo1301q86738Ytc1D/scores/";
+const apiUrl = "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IemWo1301q86738Ytc1D/scores/";
+const scorecard = document.querySelector('.leaderboard');
 
 // Post method
 const sendData = async (data) => {
-    let options = {
-        method: "POST",
-        headers: {
-            "content-type": "Application/json"
-        },
-        body: JSON.stringify(data)
-    }
+    try {
+        let options = {
+            method: "POST",
+            headers: {
+                "content-type": "Application/json"
+            },
+            body: JSON.stringify(data)
+        }
 
-    let response = await fetch(urlg, options);
-    let responseData = await response.json();
-    return responseData;
+        let response = await fetch(apiUrl, options);
+        let responseData = await response.json();
+        return responseData;
+    } catch (error) {
+        console.error('Error sending data:', error);
+        throw error;
+    }
 };
 
 // Get method
 const getData = async () => {
-    let response = await fetch(urlg);
-    let responseData = await response.json();
-    return responseData.result;
-}
-
-const scorecard = document.querySelector('.leaderboard');
+    try {
+        let response = await fetch(apiUrl);
+        let responseData = await response.json();
+        return responseData.result;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+};
 
 // Rendering data to DOM
 const render = async () => {
-    scorecard.innerHTML = '';
-    const data = await getData();
-    scorecard.innerHTML = `<tr>
-        <td class="head">Name</td>
-        <td class="head">Salary</td> <!-- Changed from 'Score' to 'Salary' -->
-    </tr>`;
-    
-    const list = data.map((item) => {
-        const tr = document.createElement('tr');
-        const td1 = document.createElement('td');
-        const td2 = document.createElement('td');
-        td1.innerHTML = item.user;
-        td2.innerHTML = item.score; // Change this line to use item.salary if it's stored as 'salary' in your API response
-        td1.setAttribute('class', 'name');
-        td2.setAttribute('class', 'score');
-        tr.appendChild(td1);
-        tr.appendChild(td2);
-        scorecard.appendChild(tr);
-    });
-}
+    try {
+        scorecard.innerHTML = '';
+        const data = await getData();
+        scorecard.innerHTML = `<thead>
+            <tr>
+                <th>Name</th>
+                <th>Salary</th>
+            </tr>
+        </thead>`;
+
+        const list = data.map((item) => {
+            const tr = document.createElement('tr');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            td1.innerHTML = item.user;
+            td2.innerHTML = item.score;
+            td1.setAttribute('class', 'name');
+            td2.setAttribute('class', 'score');
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            scorecard.appendChild(tr);
+        });
+    } catch (error) {
+        console.error('Error rendering data:', error);
+        throw error;
+    }
+};
 
 // Adding event listener
 document.addEventListener('DOMContentLoaded', () => {
-    // Add event listeners to the buttons
     document.querySelector('#refresh').addEventListener('click', async () => { render(); });
 
     document.getElementById('input-data').addEventListener('submit', async (event) => {
         event.preventDefault();
         let nameValue = document.querySelector('#name').value;
-        let salaryValue = document.querySelector('#Salary').value; // Changed from 'score' to 'Salary'
-        console.log(nameValue + " is being added with salary " + salaryValue); // Changed from 'score' to 'Salary'
+        let salaryValue = document.querySelector('#Salary').value;
+        console.log(nameValue + " is being added with salary " + salaryValue);
         let data = {
             user: nameValue,
-            score: salaryValue // Changed from 'score' to 'salary'
+            score: salaryValue
         };
-        let response = await sendData(data);
-        console.log(response);
-        render();
+        try {
+            let response = await sendData(data);
+            console.log(response);
+            render();
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+
+        // Clear input fields
+        document.querySelector('#name').value = '';
+        document.querySelector('#Salary').value = '';
     });
 
     render();
